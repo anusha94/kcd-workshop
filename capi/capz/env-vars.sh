f37f3f09ba923335ea1d520091394c326bd20f2d
Ashutosh Kumar <sonasingh46@gmail.com>
2022-04-16 03:51:54 +0530

    add capz instructions
    Signed-off-by: Ashutosh Kumar <sonasingh46@gmail.com>

---
+++ capi/capz/env-vars.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Fill in the appropriate value here
+export AZURE_SUBSCRIPTION_ID="<your-sub-id>"
+
+# Fill in the appropriate values here
+export AZURE_TENANT_ID="<your-tenant-id>"
+export AZURE_CLIENT_ID="your-client-id"
+export AZURE_CLIENT_SECRET="your-client-secret"
+
+
+# Base64 encode the variables
+export AZURE_SUBSCRIPTION_ID_B64="$(echo -n "$AZURE_SUBSCRIPTION_ID" | base64 | tr -d '\n')"
+export AZURE_TENANT_ID_B64="$(echo -n "$AZURE_TENANT_ID" | base64 | tr -d '\n')"
+export AZURE_CLIENT_ID_B64="$(echo -n "$AZURE_CLIENT_ID" | base64 | tr -d '\n')"
+export AZURE_CLIENT_SECRET_B64="$(echo -n "$AZURE_CLIENT_SECRET" | base64 | tr -d '\n')"
+
+# Settings needed for AzureClusterIdentity used by the AzureCluster
+export AZURE_CLUSTER_IDENTITY_SECRET_NAME="cluster-identity-secret"
+export CLUSTER_IDENTITY_NAME="cluster-identity"
+export AZURE_CLUSTER_IDENTITY_SECRET_NAMESPACE="default"
